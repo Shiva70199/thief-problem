d74502c89677beb561356aab3bd1704fd58efb5c
Shiva70199 <96782059+Shiva70199@users.noreply.github.com>
2025-04-26 16:09:46 +0530

    Add files via upload

---
+++ print.py
@@ -0,0 +1,11 @@
+def print_pattern():
+    pattern = [
+        "11112",
+        "32222",
+        "33334",
+        "54444",
+        "55556"
+    ]
+    for line in pattern:
+        print(line)
+print_pattern()
